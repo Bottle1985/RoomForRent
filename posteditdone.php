3a6c0c6bb7c333a5312730486648fc8ee3d94e9c
Bottle1985 <mrboongvn@yahoo.com.vn>
2022-01-19 10:00:07 +0700

    Update posteditdone.php

--- posteditdone.php
+++ posteditdone.php
@@ -1,6 +1,36 @@
 <?php
 	include_once"includes/header.php";
 	include_once"connection.php";
+	
+	//image prepare
+    if(isset($_FILES['image']))
+    {
+        $errors =array();
+        $file_name=time().$_FILES['image']['name'];
+        $file_size=$_FILES['image']['size'];
+        $file_tmp= $_FILES['image']['tmp_name'];
+        $file_type = $_FILES['image']['type'];
+        $file_end=explode('.',$_FILES['image']['name']);
+        $file_ext= strtolower(end($file_end));
+
+        $formates =array('jpeg','jpg','png');
+
+        if (in_array($file_ext, $formates)==false)
+        {
+            $errors[]="please choose jpg or png files";
+
+        }
+
+        if($file_size>2097152){
+            $errors[]="file is too large, file should be below 2 mb";
+        }
+
+        if(empty($errors)==true){
+            move_uploaded_file($file_tmp,"apartment_images/".$file_name);
+            $_POST['image']=$file_name;
+        }else {print_r($errors);
+        }
+    }
 
 	$apt_id=$_POST['id'];
 	
@@ -12,20 +42,22 @@
 	 $apt_size    =$_POST['flat_size'];
 	 $apt_no_of_rooms    =$_POST['num_of_rooms'];
 	 $apt_additional_info=$_POST['additional_info'];
-
+	 $apt_image		  =$_POST['image']	;
+	 
 	 $apartment=mysqli_query($con,"UPDATE available_flats  
 	 	set flat_city='$apt_city', 
 	 	flat_location='$apt_location',
 	 	flat_rent    ='$rent', 
 	 	availabile   ='$available' 
 	 	where flat_id='$apt_id'");
-
+	// print_r($apt_image);
 	 $apartment_details=mysqli_query($con,"UPDATE flat_details 
 	 	set  flat_city='$apt_city', 
 	 	flat_location='$apt_location',
 	 	flat_size  ='$apt_size',
 	 	num_of_rooms   ='$apt_no_of_rooms', 
-	 	additional_info='$apt_additional_info' 
+	 	additional_info='$apt_additional_info', 
+		image='$apt_image'
 	 	where flat_id  ='$apt_id' ");
 		
 ?>
