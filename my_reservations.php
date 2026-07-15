804e0dcdb1b433c893542862096dd40d0c0450b5
Bottle1985 <mrboongvn@yahoo.com.vn>
2026-07-15 14:09:29 +0700

    Update my_reservations.php

--- my_reservations.php
+++ my_reservations.php
@@ -9,6 +9,31 @@
 
 	$username = mysqli_real_escape_string($con, $_SESSION['username']);
 
+	if (empty($_SESSION['csrf_token'])) {
+		$_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+	}
+
+	if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_reservation'])) {
+		$flat_id = isset($_POST['flat_id']) ? (int) $_POST['flat_id'] : 0;
+		$csrf_token = $_POST['csrf_token'] ?? '';
+
+		if ($flat_id > 0 && hash_equals($_SESSION['csrf_token'], $csrf_token)) {
+			$statement = mysqli_prepare($con, 'DELETE FROM reserved_flats WHERE flat_id = ? AND bidder_username = ?');
+			mysqli_stmt_bind_param($statement, 'is', $flat_id, $_SESSION['username']);
+			mysqli_stmt_execute($statement);
+			mysqli_stmt_close($statement);
+			$_SESSION['reservation_message'] = 'Reservation deleted successfully.';
+		} else {
+			$_SESSION['reservation_message'] = 'Unable to delete this reservation.';
+		}
+
+		header('Location: my_reservations.php');
+		exit();
+	}
+
+	$reservation_message = $_SESSION['reservation_message'] ?? '';
+	unset($_SESSION['reservation_message']);
+
 	$query = "SELECT r.flat_id, r.bidder_username, f.flat_city, f.flat_location, f.flat_rent, f.available,
 		d.flat_size, d.num_of_rooms, d.additional_info,
 		m.first_name, m.last_name
@@ -23,6 +48,10 @@
 
 <h2>My Reserved Flats</h2>
 
+<?php if ($reservation_message): ?>
+	<p><?php echo htmlspecialchars($reservation_message); ?></p>
+<?php endif; ?>
+
 <?php if ($reservations && mysqli_num_rows($reservations) > 0): ?>
 	<div class="responsive-table">
 		<table style="border-collapse: collapse; width: 100%;">
@@ -36,6 +65,7 @@
 					<th>City</th>
 					<th>Owner</th>
 					<th>Details</th>
+					<th>Action</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -49,6 +79,13 @@
 						<td><?php echo htmlspecialchars($row['flat_city']); ?></td>
 						<td><?php echo htmlspecialchars($row['first_name'] . ' ' . $row['last_name']); ?></td>
 						<td><a href="flat_details.php?id=<?php echo $row['flat_id']; ?>">View</a></td>
+						<td>
+							<form method="post" action="my_reservations.php" onsubmit="return confirm('Delete this reservation?');">
+								<input type="hidden" name="flat_id" value="<?php echo (int) $row['flat_id']; ?>">
+								<input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
+								<button type="submit" name="delete_reservation">Delete</button>
+							</form>
+						</td>
 					</tr>
 				<?php endwhile; ?>
 			</tbody>
